d43a9a7b5f4909187a27191262684e8e38fb386d
diegosepulveda <diegoti89@gmail.com>
2016-07-27 12:39:33 -0400

    inicilizacion de la app, se instalo ui routes y se setio con el generator angular de yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name timelineApp
+ * @description
+ * # timelineApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('timelineApp', []);
